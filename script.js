2ff612c362c12c004481056ebeea5ea9b2e6df76
Anton Petrishchevskiy <antpet12@inbox.ru>
2025-10-18 23:28:53 +0300

    Add unique console messages from second developer

--- script.js
+++ script.js
@@ -4,3 +4,6 @@
 // Новое сообщение из ветки mytask
 console.log('Разработка в ветке mytask завершена успешно');
 console.log('Текущая дата: ' + new Date().toLocaleDateString());
+
+// Сообщение от второго разработчика
+console.log('⏰ Время работы: ' + new Date().toLocaleTimeString());
